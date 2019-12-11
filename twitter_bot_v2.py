fb578b79ecfb4a1b6794d3381876507f2f3c54e2
Kirk Ampong <kirkampong@fb.com>
2019-12-11 13:31:17 -0800

    Retrying using tweepy.py

---
+++ twitter_bot_v2.py
@@ -0,0 +1,13 @@
+import tweepy
+
+print("Testing python!!")
+
+CONSUMER_KEY = 'mMfyKALCanZAGKnfhGYAJJHAg '
+CONSUMER_SECRET = 'JppXnwpaacOmRNVJe7YTO712lMYts76t6xzLCuG7JnxUPhX63R'
+
+ACCESS_KEY = '1186821846529802240-qg5OlrLP76h8YKm6XnO1VumpWlqN4L'
+ACCESS_SECRET = '02fV3hsphvJUriQOqQDQ2OYoHXFguMjvxsdtnrZZd2P7F'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
