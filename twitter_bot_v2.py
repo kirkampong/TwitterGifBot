ebde921755c0e308a022c42e0146ba17eafb533f
kirkampong <kirkampong2@gmail.com>
2019-12-11 13:36:38 -0800

    Retrying using tweepy.py

--- twitter_bot_v2.py
+++ twitter_bot_v2.py
@@ -1,6 +1,6 @@
 import tweepy
 
-print("Testing python!!")
+print("My twitter bot is starting...")
 
 CONSUMER_KEY = 'mMfyKALCanZAGKnfhGYAJJHAg '
 CONSUMER_SECRET = 'JppXnwpaacOmRNVJe7YTO712lMYts76t6xzLCuG7JnxUPhX63R'
@@ -11,3 +11,5 @@ ACCESS_SECRET = '02fV3hsphvJUriQOqQDQ2OYoHXFguMjvxsdtnrZZd2P7F'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+#Use api.mentions_timeline to retrieve mentions stream..
