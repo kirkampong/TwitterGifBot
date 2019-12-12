923e3e58de31e96923265f72c691c26e9eaf8c95
kirkampong <kirkampong2@gmail.com>
2019-12-12 13:32:59 -0800

    Refining mention detection, bug fixes

--- twitter_bot_v2.py
+++ twitter_bot_v2.py
@@ -2,7 +2,7 @@ import tweepy
 
 print("My twitter bot is starting...")
 
-CONSUMER_KEY = 'mMfyKALCanZAGKnfhGYAJJHAg '
+CONSUMER_KEY = 'mMfyKALCanZAGKnfhGYAJJHAg'
 CONSUMER_SECRET = 'JppXnwpaacOmRNVJe7YTO712lMYts76t6xzLCuG7JnxUPhX63R'
 
 ACCESS_KEY = '1186821846529802240-qg5OlrLP76h8YKm6XnO1VumpWlqN4L'
@@ -12,9 +12,36 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-#Use api.mentions_timeline to retrieve mentions stream..
-mentions = api.mentions_timeline()
-for mention in mentions:
-    print(str(mention.id) + '-' + mention.text)
-    if '#randomGif' in mention.text.lower():
-        print('Mention detected... responding...')
+FILE_NAME = 'last_seen_id.txt'
+
+#Using api.mentions_timeline to retrieve mentions stream...
+#>>> api.mentions_timeline()[0].__dict__.keys()
+#    dict_keys(['_api', '_json', 'created_at', 'id', 'id_str', 'text',
+#              'truncated', 'entities', 'source', 'source_url', 'in_reply_to_status_id',
+#              'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str',
+#              'in_reply_to_screen_name', 'author', 'user', 'geo', 'coordinates', 'place',
+#               'contributors', 'is_quote_status', 'retweet_count', 'favorite_count', '
+#               favorited', 'retweeted', 'lang'])
+
+def fetch_last_seen_id(filename):
+    f_read = open(filename, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, filename):
+    f_write = open(filename, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+last_seen_id = fetch_last_seen_id(FILE_NAME)
+
+mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended')
+
+for mention in reversed(mentions):
+    print(str(mention.id) + '-' + mention.full_text)
+    last_seen_id = mention.id
+    store_last_seen_id(last_seen_id, FILE_NAME)
+    if '#randomgif' in mention.full_text.lower():
+        print('#hashtag detected... responding...')
